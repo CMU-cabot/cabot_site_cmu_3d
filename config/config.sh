503f00f2bfe1260ec03e1e65c677bf830980a2e1
Daisuke Sato <daisukes@cmu.edu>
2021-01-25 09:45:36 -0500

    change for ROS2 configuration

--- config/config.sh
+++ config/config.sh
@@ -4,12 +4,12 @@
 
 localizer=multicart
 
-map=$sitedir/maps/cmu_20200310_1f-5f.yaml
+map=$sitedir/maps/F4-NSH-Gates-Wean-T2-_2020-03-10-17-15-31.yaml
+
 anchor=
 world=$sitedir/worlds/4fr-v4.world
 global_map_name=map_global
 
 if [ $gazebo -eq 1 ]; then
-    map=$sitedir/maps/cmu_20200310_1f-5f.yaml
-    #wireless_config=$sitedir/worlds/cmu_wireless.yaml
+    map=$sitedir/maps/F4-NSH-Gates-Wean-T2-_2020-03-10-17-15-31.yaml
 fi
