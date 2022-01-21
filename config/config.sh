5f044c98bb714fb008c57b91ed0135cdd56179bb
Masayuki Murata <muratams@jp.ibm.com>
2022-01-21 20:28:46 +0900

    add data for localization in gazebo simulator
    Signed-off-by: Masayuki Murata <muratams@jp.ibm.com>

--- config/config.sh
+++ config/config.sh
@@ -10,6 +10,6 @@ world=$sitedir/worlds/4fr-v4.world
 global_map_name=map_global
 
 if [ $gazebo -eq 1 ]; then
-    map=$sitedir/maps/cmu_20200310_1f-5f.yaml
-    #wireless_config=$sitedir/worlds/cmu_wireless.yaml
+    map=$sitedir/maps/gazebo/cmu_gazebo_4F.yaml
+    wireless_config=$sitedir/worlds/cmu_wireless.yaml
 fi
